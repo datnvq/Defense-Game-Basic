947530e5cf83e2f1f32960d07191cc5e5edcf42b
datnvq <nguyenvoquocdat172005@gmail.com>
2025-03-23 01:39:21 +0700

    Create ShopitemUI Script

---
+++ Assets/QDAT/DefenseGameBasic/Script/UI/ShopitemUI.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace QDAT.DefenseBasic
+{
+    public class ShopitemUI : MonoBehaviour
+    {
+        public Text priceTxt;
+        public Image hud;
+        public Button btn;
+
+        public void UpdateUI(ShopItem item, int itemIdx)
+        {
+            if (item != null) return;
+
+            if(hud != null)
+            {
+                hud.sprite = item.previewImg;
+            }
+
+            bool isUnlocked = Pref.GetBool(Const.PLAYER_PREFIX_PREF + itemIdx);
+            if(isUnlocked)
+            {
+                if(Pref.curPlayerId == itemIdx)
+                {
+                    if(priceTxt != null)
+                    {
+                        priceTxt.text = "Active";
+                    }
+                }else if (priceTxt)
+                {
+                    priceTxt.text = "Owned";
+                }
+            }
+            else
+            {
+                if(!priceTxt)
+                {
+                    priceTxt.text = item.price.ToString();
+                }
+            }
+        }
+    }
+}
